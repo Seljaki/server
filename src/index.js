84cb3d0943ffd16b49346940c1bf02eb452f32d3
MrDog210 <tusakf@gmail.com>
2024-05-03 11:06:22 +0200

    removed ts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // src/index.js
-import express, { Express, Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import userRoutes from './routes/user.js'
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors())
 
-app.listen(port, () => {
+app.use('/user', userRoutes)
+
+app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
