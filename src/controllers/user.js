e3d7229b6c47e6293608e55a8b517661dfe69a73
MrDog210 <tusakf@gmail.com>
2024-05-07 22:22:29 +0200

    scrum-17 Implemented users CRUD

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,5 @@
 import { generateJwt } from "../utils/jwtGenerator.js"
-import { createUser } from "../utils/user.js"
+import { createUser, hashPassword } from "../utils/user.js"
 import sql from "../db.js"
 
 export async function addUser(req, res) {
@@ -18,10 +18,47 @@ export async function addUser(req, res) {
   }
 }
 
-export async function listAll(req, res) {
+export async function listAllUsers(req, res) {
   try {
     const users = await sql`SELECT id, username, email FROM users`;
     res.status(201).json({ users })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json({ message: err.message})
+  }
+}
+
+export async function updateUser(req, res) {
+  try {
+    const users = await sql`SELECT * FROM users WHERE id = ${req.user.id}`;
+    const user = users[0]
+    const { username, email, password } = req.body
+
+    if(email)
+      user.email = email
+    if(password)
+      user.password = await hashPassword(password)
+    if(username && user.username !== username) {
+      const users = await sql`SELECT * FROM users WHERE username = ${username}`;
+      if(users.length !== 0)
+        throw new Error("user with username already exists")
+      user.username = username
+    }
+
+    await sql`UPDATE users SET username = ${user.username}, password = ${user.password}, email = ${user.email} WHERE id = ${req.user.id}`
+
+    res.status(201).json({ user })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json({ message: err.message})
+  }
+}
+
+export async function deleteUser(req, res) {
+  try {
+    const userId = req.params.userId ? req.params.userId : req.user.id
+    await sql`DELETE FROM users WHERE id = ${userId}`;
+    res.status(201).json({ user })
   } catch (err) {
     console.error(err.message)
     res.status(500).json({ message: err.message})
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { requiresLogin } from '../middleware/authChecker.js';
-import { addUser, listAll } from '../controllers/user.js';
+import { addUser, deleteUser, listAllUsers, updateUser } from '../controllers/user.js';
 
 const router = express.Router()
 
-router.get('/', requiresLogin, listAll)
+router.get('/', requiresLogin, listAllUsers)
 
 router.post('/', requiresLogin, addUser)
+
+//router.put('/:userId', requiresLogin, updateUser)
+router.put('/', requiresLogin, updateUser)
+
+//router.delete('/:userId', requiresLogin, deleteUser)
+router.delete('/', requiresLogin, deleteUser)
 
 export default router
