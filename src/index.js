84185ed3f34c2e3c8d1156677ac99d5736e14301
MrDog210 <tusakf@gmail.com>
2024-05-14 20:13:32 +0200

    SCRUM-18 added basic plots REST
    need to add geo querries

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from 'cors';
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
+import plotsRoutes from './routes/plot.js'
 import morgan from "morgan";
 import { createUser } from "./utils/user.js";
 
@@ -20,6 +21,7 @@ app.use(morgan('dev'))
 // Routes
 app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
+app.use('/plots', plotsRoutes)
 
 app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
