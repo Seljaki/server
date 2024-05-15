651a49650d0662f58acbc37804e3e1d14cee6878
MrDog210 <tusakf@gmail.com>
2024-05-15 17:28:51 +0200

    SCRUM-20 addedget by id and update

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -56,36 +56,44 @@ async function getById(id) {
   return data[0]
 }
 
+export async function getjobById(req, res) {
+  try {
+    const jobId = req.params.jobId
+
+    res.status(StatusCodes.OK).json({ job: await getById(jobId) })
+  } catch (err) {
+    console.error(err.message)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message})
+  }
+}
+
 export async function updateJob(req, res) {
   try {
-    const invoiceId = req.params.invoiceId
     const jobId = req.params.jobId
     const j = await getById(jobId)
 
     const { quantity = j.quantity, timeTaken = j.timeTaken, jobtype_id = j.jobtype_id } = req.body
-    let { price } = req.body
+    let { price = j.price, totalPrice = j.totalPrice } = req.body
 
-    const jobType = await getJobTypeByIdJson(jobtype_id)
-    if(!price) {
-      price = jobType.price
+    if(j.price !== price || jobtype_id !== j.jobtype_id || quantity !== j.quantity) {
+      console.log('jobtype_id: ' + jobtype_id)
+      const jobType = await getJobTypeByIdJson(jobtype_id)
+      if(jobtype_id !== j.jobtype_id)
+        price = jobType.price
+      if(jobType.quantityType === 'price_per_hour') {
+        totalPrice = price * (quantity/60.0)
+      } else {
+        totalPrice = quantity * price
+      }
     }
 
-    let totalPrice = price
-    if(jobType.quantityType === 'price_per_hour') {
-      totalPrice = price * (quantity/60.0)
-    } else {
-      totalPrice = quantity * price
-    }
-
-    const jobs = await sql`INSERT INTO job VALUES (
-      DEFAULT,
-      ${quantity},
-      ${price},
-      ${totalPrice},
-      ${timeTaken},
-      ${invoiceId},
-      ${jobtype_id}
-    ) returning *`
+    const jobs = await sql`UPDATE job SET
+      quantity = ${quantity},
+      price = ${price},
+      "totalPrice" = ${totalPrice},
+      "timeTaken" = ${timeTaken},
+      jobtype_id = ${jobtype_id}
+    WHERE id = ${jobId} returning *`
 
     res.status(StatusCodes.OK).json({ job: jobs[0] })
   } catch (err) {
